113b8b471c910f0ee07f30056ed0703433293609
Adriel Arias <30638262+adriel3005@users.noreply.github.com>
2022-11-29 22:12:03 -0600

    Modify hardcoded summary values
    Testing CICD and deployment failures

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace HealthApplication.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "LukeWarm", "Balmy", "Fiery", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
