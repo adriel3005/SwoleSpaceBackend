77079276383d4bb403e69b5e17fc62d3f45e7164
Adriel Arias <30638262+adriel3005@users.noreply.github.com>
2023-01-06 21:12:47 -0600

    Add updated_at column to userprofile model

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -6,6 +6,7 @@ namespace HealthApplication.Models
     {
         public Guid id { get; set; }
         public string username { get; set; }
+        public DateTime updated_at { get; set; }
         public string? full_name { get; set; }
         public string? avatar_url { get; set; }
         public string? website { get; set; }
