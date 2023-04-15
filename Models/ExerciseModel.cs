1ea4a52da3caf0c12f335a7fe145f0d402d2ef06
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-04-14 21:27:57 -0500

    Add endpoint to retrieve exercises
    Add DB logic to get exercises from defined table
    
    Ref #6

---
+++ Models/ExerciseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthApplication.Models
+{
+    public class ExerciseModel
+    {
+        public int exercise_id { get; set; }
+        public string exercise_name { get; set; }
+        public string exercise_type { get; set; }
+        public string exercise_description { get; set; }
+        public string created_at { get; set; }
+    }
+}
