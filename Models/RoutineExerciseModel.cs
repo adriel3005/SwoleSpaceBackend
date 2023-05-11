bad0a28a9ad7013a0a3f7ab8be63855eb1cbd73b
adriel3005 <30638262+adriel3005@users.noreply.github.com>
2023-05-10 21:18:26 -0500

    Update Routine exercise Model
    Ref #19

--- Models/RoutineExerciseModel.cs
+++ Models/RoutineExerciseModel.cs
@@ -7,6 +7,7 @@
         public int exercise_id { get; set; }
         public int repetition { get; set; }
         public int sets { get; set; }
+        public int default_weight { get; set; }
         public DateTime? created_at { get; set; }
         public Guid user_id { get; set; }
         public int exercise_order { get; set; }
